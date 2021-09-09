9139a3082b6a364cc1d66f33c4b7d79d87fbb7ba
Marcelo <3338@holbertonschool.com>
2021-09-09 17:53:46 +0000

    D

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,11 +5,12 @@ if __name__ == "__main__":
     operators = ['+', '-', '*', '/']
     argv = sys.argv[1:]
     leni = len(argv)
-
-    if leni != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
-    else:
+    print(leni)
+#    if leni != 3:
+#        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+#        exit(1)
+#    else:
+    if leni == 3:
         for i in operators:
             if sys.argv[2] == i:
                 a = int(sys.argv[1])
@@ -17,9 +18,19 @@ if __name__ == "__main__":
 
                 if sys.argv[2] == '+':
                     print("{} {} {} = {}".format(a, sys.argv[2], b, add(a, b)))
+                    exit(1)
                 elif sys.argv[2] == '-':
                     print("{} {} {} = {}".format(a, sys.argv[2], b, sub(a, b)))
+                    exit(1)
                 elif sys.argv[2] == '*':
                     print("{} {} {} = {}".format(a, sys.argv[2], b, mul(a, b)))
+                    exit(1)
                 elif sys.argv[2] == '/':
                     print("{} {} {} = {}".format(a, sys.argv[2], b, div(a, b)))
+                    exit(1)
+                else:
+                    print("Unknown operator. Available operators: +, -, * and /")
+                    exit(1)
+    else:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
