add4341f0d1016b9cc85eb92763603fd5474bc9a
Marcelo <3338@holbertonschool.com>
2021-09-09 18:01:12 +0000

    F

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,35 +2,24 @@
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     import sys
-    operators = ['+', '-', '*', '/']
+    operators = ["+", "-", "*", "/"]
     argv = sys.argv[1:]
     leni = len(argv)
-    print(leni)
-#    if leni != 3:
-#        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-#        exit(1)
-#    else:
-    if leni == 3:
-        for i in operators:
-            if sys.argv[2] == i:
-                a = int(sys.argv[1])
-                b = int(sys.argv[3])
-
-                if sys.argv[2] == '+':
-                    print("{} {} {} = {}".format(a, sys.argv[2], b, add(a, b)))
-                    exit(1)
-                elif sys.argv[2] == '-':
-                    print("{} {} {} = {}".format(a, sys.argv[2], b, sub(a, b)))
-                    exit(1)
-                elif sys.argv[2] == '*':
-                    print("{} {} {} = {}".format(a, sys.argv[2], b, mul(a, b)))
-                    exit(1)
-                elif sys.argv[2] == '/':
-                    print("{} {} {} = {}".format(a, sys.argv[2], b, div(a, b)))
-                    exit(1)
-                else:
-                    print("Unknown operator. Available operators: +, -, * and /")
-                    exit(1)
-    else:
+    if leni != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
+    elif sys.argv[2] not in operators:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    else:
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+
+        if sys.argv[2] == "+":
+            print("{} {} {} = {}".format(a, sys.argv[2], b, add(a, b)))
+        elif sys.argv[2] == "-":
+            print("{} {} {} = {}".format(a, sys.argv[2], b, sub(a, b)))
+        elif sys.argv[2] == "*":
+            print("{} {} {} = {}".format(a, sys.argv[2], b, mul(a, b)))
+        elif sys.argv[2] == "/":
+            print("{} {} {} = {}".format(a, sys.argv[2], b, div(a, b)))
