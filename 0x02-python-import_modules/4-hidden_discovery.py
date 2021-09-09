e7f2bc6909e3ff328f1b5803f0c3b7d6a59ad3e2
Marcelo <3338@holbertonschool.com>
2021-09-09 18:58:04 +0000

    D

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     import hidden_4 as file
     list = dir(file)
     for i in list:
-        if i[0] is not "_" and i[1] is not "_":
+        if i[0] != "_" and i[1] != "_":
             print(i)
