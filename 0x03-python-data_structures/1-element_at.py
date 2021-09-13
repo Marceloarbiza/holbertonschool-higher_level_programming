00a0353e3859f0e8353249e836e87a912050dc6b
Marcelo <3338@holbertonschool.com>
2021-09-13 16:53:07 +0000

    D

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    leni = len(my_list)
+    leni = len(my_list) - 1
     if idx < 0:
         return None
     elif idx > leni:
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
-        print("{}" .format(matrix[i]))
+        #print("{}".format(matrix[i]))
+        print("{}" .format(str(matrix[i])[1:-1]))
