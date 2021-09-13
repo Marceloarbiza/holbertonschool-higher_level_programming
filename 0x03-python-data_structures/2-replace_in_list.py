6e0ab2e3f246b3b87253d919d8b23d5b89096abf
Marcelo <3338@holbertonschool.com>
2021-09-13 16:55:00 +0000

    D

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    leni = len(my_list)
+    leni = len(my_list) - 1
     if idx < 0:
         return my_list
     elif idx > leni:
--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
 
-    leni = len(my_list)
+    leni = len(my_list) - 1
     tmp_list = my_list[:]
 
     if idx < 0:
