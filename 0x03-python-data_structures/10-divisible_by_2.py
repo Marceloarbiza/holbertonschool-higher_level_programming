2b1e6e43fee1ce621520f9c9b6b682cfddc923f9
Marcelo <3338@holbertonschool.com>
2021-09-13 18:58:07 +0000

    D

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    if not my_list:
+        return None
+
+    tf_list = []
+    for i in my_list:
+        if i % 2 == 0:
+            tf_list.append(True)
+        else:
+            tf_list.append(False)
+    return tf_list
