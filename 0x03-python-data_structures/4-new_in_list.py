b2efec41c69d04905ce53b57f0c29ebed722186e
Marcelo <3338@holbertonschool.com>
2021-09-13 15:00:13 +0000

    D

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+
+    leni = len(my_list)
+    tmp_list = my_list[:]
+
+    if idx < 0:
+        return tmp_list
+    elif idx > leni:
+        return tmp_list
+    else:
+        tmp_list[idx] = element
+        return tmp_list
