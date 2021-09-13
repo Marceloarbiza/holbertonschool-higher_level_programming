2aa940d46c8d9d863907a6cede12013578e4638a
Marcelo <3338@holbertonschool.com>
2021-09-13 18:39:10 +0000

    D

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     maxi = 0
-    if my_list == []:
+    bol = True
+    if bool(my_list) == False:
         return None
     else:
         for i in my_list:
