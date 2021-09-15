5aadadba1883f3fb77f02f4b2cbf18b024a50d02
Marcelo <3338@holbertonschool.com>
2021-09-15 16:36:01 +0000

    D

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,9 @@
 #!/usr/bin/puthon3
 def uniq_add(my_list=[]):
     new_list = []
-    new_list = set(my_list) 
+    res = 0
+    for i in my_list:
+        if i not in new_list:
+            new_list.append(i)
     res = sum(new_list)
     return res
