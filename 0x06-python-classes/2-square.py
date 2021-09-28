49f345f634351e15f9f8bd97608be22947afeaeb
Marceloarbiza <3338@holbertonschool.com>
2021-09-28 15:16:29 -0700

    D

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""Doc"""
+
+
 class Square:
+    """Define Exceptions for size"""
+
     def __init__(self, size=0):
         if isinstance(size, int) is False:
             raise TypeError("size must be an integer")
