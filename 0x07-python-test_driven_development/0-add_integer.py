95bfcf09f753c445c2a62935d9eec23c5b37a2f8
Marceloarbiza <3338@holbertonschool.com>
2021-10-03 12:53:09 -0700

    D

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,13 +1,24 @@
 #!/usr/bin/python3
 """
-    Add integer module
+    Add two numbers (integer or float)
 """
 
 
 def add_integer(a, b=98):
+    """
+    This function receives 2 numbers as arguments.
+
+    The numbers must be integer or float.
+
+    In the case of receiving only one number, \
+        the other number will be 98 by default.
+
+    This function will always return the result \
+        as an integer, regardless of receiving integer or float.
+    """
     if type(a) not in [int, float]:
-        raise TypeError ("a must be an integer")
+        raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
-        raise TypeError ("b must be an integer")
-    
+        raise TypeError("b must be an integer")
+
     return int(a) + int(b)
