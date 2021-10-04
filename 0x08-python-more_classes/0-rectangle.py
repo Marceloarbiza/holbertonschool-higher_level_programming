7ad43fec3402e472777cbfb9c7398113027a22c6
Marceloarbiza <3338@holbertonschool.com>
2021-10-04 06:26:44 -0700

    D

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+    Write an empty class Rectangle that defines a rectangle.
+"""
+
+
+class Rectangle:
+    """
+        This class represent an empty rectangle
+    """
+    pass
