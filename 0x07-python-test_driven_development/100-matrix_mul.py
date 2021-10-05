ab95550e08331588ce0418331b6cfe8c1db1a81b
Marceloarbiza <3338@holbertonschool.com>
2021-10-05 15:53:03 -0700

    D

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -20,4 +20,11 @@ def matrix_mul(m_a, m_b):
         if isinstance(lb, list) is False:
             raise TypeError("m_b must be a list of lists")
 
+    if m_a == [] or m_a == [[]]:
+        raise ValueError("m_a can't be empty")
+    if m_b == [] or m_b == [[]]:
+        raise ValueError("m_b can't be empty")
 
+    for la in m_a:
+        if [(type(i) is not int or type(i) is not float) for i in la]:
+            raise TypeError("m_a should contain only integers or floats")
