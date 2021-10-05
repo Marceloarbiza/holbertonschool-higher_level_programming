afc8583701432c817b086ba276077add7eb4036c
Marceloarbiza <3338@holbertonschool.com>
2021-10-05 06:13:40 -0700

    D

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,48 +1,52 @@
 #!/usr/bin/python3
 """Unittest for max_integer([..])
 """
+
+
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
-
-    def basic_case(self):
+    """
+        Test cases for max_integer
+    """
+    def test_basic_case(self):
         """ Test the base case
         """
         self.assertEqual(max_integer([1, 2, 8, 4]), 8)
 
-    def same_number(self):
+    def test_same_number(self):
         """ Test the same number
         """
         self.assertEqual(max_integer([4, 4, 4, 4]), 4)
 
-    def empty_list(self):
+    def test_empty_list(self):
         """ Test empty list
         """
         self.assertEqual(max_integer([]), None)
 
-    def none_list(self):
+    def test_none_list(self):
         """ Test None list
         """
         self.assertEqual(max_integer([None]), None)
 
-    def pos_neg(self):
+    def test_pos_neg(self):
         """ Test positive and negative numbers
         """
         self.assertEqual(max_integer([2, -3, 1, -4]), 2)
 
-    def char_letters(self):
+    def test_char_letters(self):
         """ Test char letters
         """
         self.assertEqual(max_integer(['b', 'a', 'f', 'c']), 'f')
 
-    def char_int(self):
+    def test_char_int(self):
         """ Test char int
         """
         self.assertRaises(TypeError, max_integer, [2, 'f', 5, 'r'])
 
-    def char_string(self):
+    def test_char_string(self):
         """ Test string in list
         """
         self.assertRaises(TypeError, max_integer, [2, "dog", 5, "cat"])
