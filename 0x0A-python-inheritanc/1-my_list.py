26a17d6cb6f1f962633f9999a8307042922e8d31
Marceloarbiza <3338@holbertonschool.com>
2021-10-12 07:53:36 -0700

    D

---
+++ 0x0A-python-inheritanc/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Write a class MyList that inherits from list
+"""
+
+
+class MyList(list):
+    """ sorted ordena de forma creciente la lista """
+    def print_sorted(self):
+        print(sorted(self))
