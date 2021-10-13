f163169c37d6a4944f384c421d84161775b68669
Marceloarbiza <3338@holbertonschool.com>
2021-10-12 17:01:34 -0700

    D

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
-""" Write an empty class BaseGeometry.
+"""
+==============
+Module class BaseGeometry
+==============
 """
 
 
 class BaseGeometry:
+    """ empty class BaseGeometry """
+
     pass
