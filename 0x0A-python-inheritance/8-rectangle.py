d7061be7bcd636b7789767164226f21300b65ab1
Marceloarbiza <3338@holbertonschool.com>
2021-10-12 17:07:05 -0700

    D

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-""" Write a class BaseGeometry (based on 6-base_geometry.py).
+"""
+==============================================
+Module class def __init__(self, width, height)
+==============================================
 """
 
 
 class BaseGeometry:
-    """ functions """
+    """ definition of functions """
+
     def area(self):
         raise Exception("area() is not implemented")
 
@@ -15,11 +19,13 @@ class BaseGeometry:
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
 
+
 class Rectangle(BaseGeometry):
     """ Class Rectangle heridate from BaseGeometry """
+
     def __init__(self, width, height):
-        
-            self.__width = width
-            self.integer_validator("width", width)
-            self.__height = height
-            self.integer_validator("height", height)
+
+        self.__width = width
+        self.integer_validator("width", width)
+        self.__height = height
+        self.integer_validator("height", height)
