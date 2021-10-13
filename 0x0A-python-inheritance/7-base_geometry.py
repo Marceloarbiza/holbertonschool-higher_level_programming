cde47897d4c710ca7a224e828245099efb45dd78
Marceloarbiza <3338@holbertonschool.com>
2021-10-12 19:02:10 -0700

    D

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-""" Write a class BaseGeometry (based on 6-base_geometry.py).
+"""
+===============================
+Module class 7-base_geometry.py
+===============================
 """
 
 
 class BaseGeometry:
-    """ functions """
+    """ functions of BaseGeometry """
+
     def area(self):
         raise Exception("area() is not implemented")
 
