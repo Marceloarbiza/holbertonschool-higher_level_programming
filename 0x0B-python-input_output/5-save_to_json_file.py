2800f2b25f42d6536ab35ff89c1a1cc42be531fa
Marceloarbiza <3338@holbertonschool.com>
2021-10-13 10:03:36 -0700

    D

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import json
+"""
+==========================================================================
+function that writes an Object to a text file, using a JSON representation
+==========================================================================
+"""
+
+
+def save_to_json_file(my_obj, filename):
+    """ The dump() function is used to serialize data.
+        It takes a Python object, serializes it and writes
+        the output (which is a JSON string) to a file like object.
+    """
+
+    with open(filename, 'w') as outfile:
+        return json.dump(my_obj, outfile)
