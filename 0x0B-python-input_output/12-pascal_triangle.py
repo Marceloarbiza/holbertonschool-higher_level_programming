1dec8ad0da00afe8f8dd2cd7896293854ce7a47e
Marceloarbiza <3338@holbertonschool.com>
2021-10-13 19:03:28 -0700

    D

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,14 +1,25 @@
 #!/usr/bin/python3
+"""
+================
+pascala triangle
+================
+"""
 
-_list = []
 
 def pascal_triangle(n):
-    
-    for i in range(n):
-        print("1", end="")
-        for j in range(n):
-            print("#", end="")
-        for
-        print()
+    """ pascal triangle function """
 
-    return _list
+    mat = []
+
+    for m in range(n):
+        mat.append([])
+
+    for i in range(n):
+        for j in range(i + 1):
+            if i == j or j == 0:
+                mat[i].append(1)
+            else:
+                res = mat[i-1][j] + mat[i-1][j-1]
+                mat[i].append(res)
+
+    return mat
