d69acd7681c79becacba39dcb0ca7166e16d612e
Marceloarbiza <3338@holbertonschool.com>
2021-10-13 18:00:38 -0700

    D

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+_list = []
+
+def pascal_triangle(n):
+    
+    for i in range(n):
+        print("1", end="")
+        for j in range(n):
+            print("#", end="")
+        for
+        print()
+
+    return _list
---
+++ 0x0B-python-input_output/12-pascal_trianglecpy.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+===========================================================================
+returns a list of lists of integers representing the Pascal’s triangle of n
+===========================================================================
+"""
+
+
+def pascal_triangle(n):
+    """ Technical interview preparation """
+    
+    _list = []
+
+    for i in range(n):
+        print("#")
+        for j in range(n):
+            print("#", end="")
+
+    return _list
+
