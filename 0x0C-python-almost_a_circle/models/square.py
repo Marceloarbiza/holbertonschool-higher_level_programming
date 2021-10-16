db28070345a7261dec8dbd9a685938e9f038107f
Marceloarbiza <3338@holbertonschool.com>
2021-10-16 12:50:42 -0700

    D

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+=========================================================
+Creation of the class Square that inherits from Rectangle
+=========================================================
+"""
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """ class Square inherits from Rectangle """
+
+    def __init__(self, size, x=0, y=0, id=None):
+
+
