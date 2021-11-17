31f8bca80a15632d66e57182a90114361a9f3284
Marceloarbiza <3338@holbertonschool.com>
2021-11-17 13:24:01 -0800

    D

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -2,4 +2,4 @@
 -- because Batich 3 is the best!
 SELECT tv_shows.title,tv_show_genres.genre_id FROM tvshows
 JOIN tvshows ON tv_shows.title = tv_show_genres.genre_id
-ORDER BY tv_shows.title,tv_show_genres.genre_id ASC;
+ORDER BY title,genre_id ASC;
