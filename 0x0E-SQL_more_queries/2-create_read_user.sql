6448ec3cdb5b38edfadf5fc369f6fa6ec045aa86
Marceloarbiza <3338@holbertonschool.com>
2021-11-17 10:43:21 -0800

    D

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,6 +2,5 @@
 -- because Batich 3 is the best!
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 USE hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
-IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
