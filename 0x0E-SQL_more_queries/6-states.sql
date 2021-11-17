d45f2af71a942c81878dc04f7488a49713f0dcb7
Marceloarbiza <3338@holbertonschool.com>
2021-11-17 11:04:22 -0800

    D

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,5 @@
+-- 3 first students in the Batch ID=3
+-- because Batich 3 is the best!
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (id INT UNIQUE NOT NULL PRIMARY KEY, name VARCHAR(256) NOT NULL);
