d481725bd64df58bdd33f14ecb551cec2507096b
Marceloarbiza <3338@holbertonschool.com>
2021-11-17 18:34:26 -0800

    D

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,6 +1,13 @@
 -- 3 first students in the Batch ID=3
 -- because Batich 3 is the best!
-SELECT title,name FROM tv_shows
-RIGHT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-ORDER BY title ASC;
+--SELECT title,name FROM tv_shows
+--RIGHT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+--LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+--ORDER BY title ASC;
+SELECT tv_shows.title AS title, tv_genres.name AS name
+FROM tv_show_genres
+RIGHT JOIN tv_shows
+ON tv_show_genres.show_id = tv_shows.id
+LEFT JOIN tv_genres
+ON tv_genres.id = tv_show_genres.genre_id
+ORDER BY title, name ASC;
