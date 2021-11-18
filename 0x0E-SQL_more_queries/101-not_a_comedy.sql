a294b06dbac14e2485273cc3d37ee3d5110939cc
Marceloarbiza <3338@holbertonschool.com>
2021-11-17 18:48:27 -0800

    D

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,10 +1,10 @@
 -- 3 first students in the Batch ID=3
 -- because Batich 3 is the best!
 SELECT title
-FROM tv_show WHERE tv_shows.id NOT IN (
-      SELECT tv_genres.id
+FROM tv_shows WHERE tv_shows.id NOT IN (
+      SELECT tv_shows.id
       FROM tv_shows
-      INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+      INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
       INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
       WHERE tv_genres.name = 'Comedy')
 ORDER BY title;
