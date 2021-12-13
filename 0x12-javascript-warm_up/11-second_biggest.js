ca2ba53ed9646a382e2c8fad07edff13d6908658
Marcelo <3338@holbertonschool.com>
2021-12-13 11:58:08 -0800

    11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -7,7 +7,7 @@ if (args.length > 3) {
   for (let i = 2; i < args.length; i++) {
     li.push(parseInt(args[i]));
   }
-  li.sort();
+  li.sort(function (a, b) { return a - b; });
   console.log(parseInt(li[li.length - 2]));
 } else {
   console.log(0);
