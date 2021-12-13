324180117ad10dd09846a5f69efde2aec03cd33c
Marcelo <3338@holbertonschool.com>
2021-12-13 11:53:20 -0800

    11f

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,10 +2,13 @@
 
 const args = process.argv;
 
-if (args[2] == null || parseInt(args.length) < 3) {
-  console.log(0);
+if (args.length > 3) {
+  const li = [];
+  for (let i = 2; i < args.length; i++) {
+    li.push(parseInt(args[i]));
+  }
+  li.sort();
+  console.log(parseInt(li[li.length - 2]));
 } else {
-  args.splice(0, 2);
-  args.sort();
-  console.log(parseInt(args[args.length - 2]));
+  console.log(0);
 }
