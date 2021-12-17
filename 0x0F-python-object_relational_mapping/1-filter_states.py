314b2003982a82755710ed967cb4d1a775003d05
Marcelo <3338@holbertonschool.com>
2021-12-17 15:22:03 -0800

    5

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -3,11 +3,12 @@
 import MySQLdb
 import sys
 
-args = sys.argv
-database_connection = MySQLdb.connect(host='localhost', port=3306, user= args[1], passwd=args[2], db=args[3])
-cur = database_connection.cursor()
-cur.execute( "SELECT * FROM states WHERE states.name regexp '^N' ORDER BY states.id" )
-for row in cur.fetchall() :
-    print(row)
-cur.close()
-database_connection.close()
+if __name__ == "__main__":
+	args = sys.argv
+	database_connection = MySQLdb.connect(host='localhost', port=3306, user= args[1], passwd=args[2], db=args[3])
+	cur = database_connection.cursor()
+	cur.execute( "SELECT * FROM states WHERE states.name regexp '^N' ORDER BY states.id" )
+	for row in cur.fetchall() :
+		print(row)
+	cur.close()
+	database_connection.close()
--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -3,11 +3,18 @@
 import MySQLdb
 import sys
 
-args = sys.argv
-database_connection = MySQLdb.connect(host='localhost', port=3306, user= args[1], passwd=args[2], db=args[3])
-cur = database_connection.cursor()
-cur.execute("SELECT C.name FROM cities C JOIN states S ON C.state_id = S.id WHERE S.name = (%s) ORDER BY C.id", (args[4],))
-for name in cur.fetchall() :
-    print(name)
-cur.close()
-database_connection.close()
+if __name__ == "__main__":
+	args = sys.argv
+	database_connection = MySQLdb.connect(host='localhost', port=3306, user= args[1], passwd=args[2], db=args[3])
+	cur = database_connection.cursor()
+	cur.execute("SELECT C.name FROM cities C JOIN states S ON C.state_id = S.id WHERE S.name = (%s) ORDER BY C.id", (args[4],))
+	rows = cur.fetchall()
+	i = len(rows)
+	for j in range(i - 1):
+		print(f"{rows[j][0]}, ", end="")
+	if (i != 0):
+		print(f"{rows[j + 1][0]}")
+	else:
+		print()
+	cur.close()
+	database_connection.close()
