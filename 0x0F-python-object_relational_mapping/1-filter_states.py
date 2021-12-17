415013d6e580cb9edc9e11995fb5866555c86e8d
Marcelo <3338@holbertonschool.com>
2021-12-17 10:33:17 -0800

    1 in da house

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,7 +6,7 @@ import sys
 args = sys.argv
 database_connection = MySQLdb.connect(host='localhost', port=3306, user= args[1], passwd=args[2], db=args[3])
 cur = database_connection.cursor()
-cur.execute( "SELECT * FROM states ORDER BY states.id" )
+cur.execute( "SELECT * FROM states WHERE states.name regexp '^N' ORDER BY states.id" )
 for row in cur.fetchall() :
     print(row)
 cur.close()
