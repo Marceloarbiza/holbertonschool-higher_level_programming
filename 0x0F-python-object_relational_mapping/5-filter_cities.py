f5720be424b8365aa4ffd3e7e5f9fdc23566acb4
Marcelo <3338@holbertonschool.com>
2021-12-18 08:51:42 -0800

    D

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
+""" Write a script that takes in the name of a state """
 
-import MySQLdb
-import sys
 
 if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
     args = sys.argv
-    database_connection = MySQLdb.connect(host='localhost', port=3306, user=args[1], passwd=args[2], db=args[3])
+    database_connection = MySQLdb.connect(host='localhost', port=3306,
+                                               user=args[1], passwd=args[2],
+                                               db=args[3])
     cur = database_connection.cursor()
-    cur.execute("SELECT C.name FROM cities C JOIN states S ON C.state_id = S.id WHERE S.name = (%s) ORDER BY C.id", (args[4],))
+    cur.execute("SELECT C.name FROM cities C JOIN states S ON C.state_id\
+                = S.id WHERE S.name = (%s) ORDER BY C.id", (args[4],))
     rows = cur.fetchall()
     i = len(rows)
     for j in range(i - 1):
