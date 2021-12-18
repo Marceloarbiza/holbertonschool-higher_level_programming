83139df6487afc91958180cd9560db05c10865e7
Marcelo <3338@holbertonschool.com>
2021-12-18 08:46:10 -0800

    F

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
-
-import MySQLdb
-import sys
+""" Write a script that lists all cities from the database hbtn_0e_4_usa """
 
 if __name__ == "__main__":
-	args = sys.argv
-	database_connection = MySQLdb.connect(host='localhost', port=3306, user= args[1], passwd=args[2], db=args[3])
-	cur = database_connection.cursor()
-	cur.execute("SELECT C.id, C.name, S.name FROM cities C JOIN states S ON C.state_id = S.id ORDER BY C.id")
-	for row in cur.fetchall() :
-		print(row)
-	cur.close()
-	database_connection.close()
+    import MySQLdb
+    import sys
+
+    args = sys.argv
+    database_connection = MySQLdb.connect(host='localhost', port=3306,
+                                               user=args[1], passwd=args[2],
+                                               db=args[3])
+    cur = database_connection.cursor()
+    cur.execute("SELECT C.id, C.name, S.name FROM cities C\
+                JOIN states S ON C.state_id = S.id ORDER BY C.id")
+    for row in cur.fetchall():
+        print(row)
+    cur.close()
+    database_connection.close()
