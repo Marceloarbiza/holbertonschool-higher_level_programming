adaa6dcb08fbe3ae8caa0a755c2c2fcb7b7ff3fb
Marcelo <3338@holbertonschool.com>
2021-12-18 05:39:47 -0800

    D

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -7,7 +7,7 @@ Base = declarative_base()
 
 
 class State(Base):
-	""" Class State """
-	__tablename__ = 'states'
-	id = Column(Integer, primary_key=True, nullable=False)
-	name =  Column(String(128), nullable=False)
+    """ Class State """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
