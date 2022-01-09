954fc21caa4cecd9dcd75a37af624ee60b0039f0
Marceloarbiza <3338@holbertonschool.com>
2022-01-09 23:59:40 +0000

    D

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -3,13 +3,13 @@
 Write a Python script that takes in a URL, sends a request to the URL
 """
 
-import requests
+from requests import get
 import sys
 
 
 if __name__ == "__main__":
     url = sys.argv[1]
     obj = 'X-Request-Id'
-    r = requests.get(url)
+    r = get(url)
     value = r.headers[obj]
     print(value)
