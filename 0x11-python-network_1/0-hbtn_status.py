0d626eddb6b7e59d6be32569cd73f88130b652e7
Marceloarbiza <3338@holbertonschool.com>
2022-01-10 01:18:35 +0000

    0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
+"""
+Write a Python script that takes in a URL, sends a request to the URL
+"""
+import urllib.request
+import sys
 
-import urllib
 
-
-
+if __name__ == "__main__":
+    url = "https://intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as req:
+        r = req.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(r)))
+        print("\t- content: {}".format(r))
+        print("\t- utf8 content: {}".format(r.decode('utf8')))
