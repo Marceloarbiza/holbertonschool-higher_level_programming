af2c67ea2e2ff46690f2f8c342f108cdc3d02d1e
Marceloarbiza <3338@holbertonschool.com>
2022-01-10 00:13:55 +0000

    6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -3,19 +3,11 @@
 Write a Python script that takes in a URL, sends a request to the URL
 """
 
-import urllib.request
 import sys
-import urllib.parse
+from requests import post
 
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    data = {}
-    data['email'] = sys.argv[2]
-    url_values = urllib.parse.urlencode(data)
-    values_ascii = url_values.encode('ascii')
-    req = urllib.request.Request(url, values_ascii)
-    with urllib.request.urlopen(req) as r:
-        read_r = r.read()
-        read_r = read_r.decode('utf-8')
-        print(read_r)
+    value = post(url, data={'email': sys.argv[2]})
+    print(value)
