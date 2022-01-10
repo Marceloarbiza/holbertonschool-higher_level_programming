5460f644f5c2f7ec61e40794d93862dc3a99d6ec
Marceloarbiza <3338@holbertonschool.com>
2022-01-10 02:54:49 +0000

    100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -4,17 +4,19 @@ Write a Python script that takes in a URL, sends a request to the URL
 """
 
 from requests import get
-from requests import post
-import sys
+from sys import argv
 
 if __name__ == "__main__":
-    res_json = get("https://api.github.com/repos/{}/{}/commits"\
-                    .format(argv[2], argv[1])).json()
+    init = "https://api.github.com/repos/{}/{}/commits"\
+            .format(argv[2], argv[1])
+    r_g = get(init)
+    commits = r_g.json()
 
     try:
-        for x in range(10):
-            prin('{}: {}'.format(res_json[x].get('sha'),
-                  res_json[x].get('commit')
-                  .get('authorÂ').get('name')))
+        for count in range(10):
+            print("{}: {}".format(commits[count]
+                  .get("sha"), commits[count].get("commit")
+                  .get("author").get("name")))
+
     except IndexError:
         pass
