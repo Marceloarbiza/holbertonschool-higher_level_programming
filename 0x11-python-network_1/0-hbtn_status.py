145c42ada44d4c7a3b940e29d985a8619c1f2025
Marceloarbiza <3338@holbertonschool.com>
2022-01-10 01:41:30 +0000

    0f

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,19 +2,13 @@
 """
 Write a Python script that takes in a URL, sends a request to the URL
 """
-import urllib.request
-import sys
+from urllib import request
 
 
-def fetcher():
-    """ fetch """    
-    url = "https://intranet.hbtn.io/status"
-    with urllib.request.urlopen(url) as req:
+if __name__ == "__main__":
+    with request.urlopen("https://intranet.hbtn.io/status") as req:
         r = req.read()
         print("Body response:")
         print("\t- type: {}".format(type(r)))
         print("\t- content: {}".format(r))
         print("\t- utf8 content: {}".format(r.decode('utf8')))
-
-if __name__ == "__main__":
-    fetcher()
