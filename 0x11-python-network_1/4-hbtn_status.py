283c9cd1b2483bd9cb1ceeaa825734755cabdada
Marceloarbiza <3338@holbertonschool.com>
2022-01-10 01:02:26 +0000

    D

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,14 +2,12 @@
 """
 Write a Python script that takes in a URL, sends a request to the URL
 """
-
-
 from requests import get
 
 
 if __name__ == "__main__":
-    url = get('https://intranet.hbtn.io/status')
+    url = "https://intranet.hbtn.io/status"
     r = get(url)
-    print('Body response:')
-    print('\t- type: {}'.format(r.text))
-    print('\t- content: {}'.format(r.content))
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
