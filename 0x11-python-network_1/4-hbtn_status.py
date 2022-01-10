56afc4801e3739034e60a5010ff896ad84bf603f
Marceloarbiza <3338@holbertonschool.com>
2022-01-10 00:45:19 +0000

    4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Write a Python script that takes in a URL, sends a request to the URL
+"""
+
+
+from requests import get
+
+
+if __name__ == "__main__":
+    url = get('https://intranet.hbtn.io/status')
+    r = get(url)
+    print('Body response:')
+    print('\t- type: {}'.format(r.text))
+    print('\t- content: {}'.format(r.text))
