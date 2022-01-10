4ddb3638ed929d6c89e32849d0eb050140f8d9a2
Marceloarbiza <3338@holbertonschool.com>
2022-01-10 00:47:20 +0000

    D

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -12,4 +12,4 @@ if __name__ == "__main__":
     r = get(url)
     print('Body response:')
     print('\t- type: {}'.format(r.text))
-    print('\t- content: {}'.format(r.text))
+    print('\t- content: {}'.format(r.content))
