ad5590b5b93d0a064352f03e62f7a229de89f75b
Marceloarbiza <3338@holbertonschool.com>
2022-02-01 16:31:43 +0000

    LGF

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,19 +2,16 @@
 /* Write a script that display the status code of a GET request. */
 /* HTTP request lo hacemos con la libreria Axios */
 /* traigo el arg que será el nombre de la url */
-/* el contenido a escribir en el archivo */
-/* el módulo fs sirve para acceder e interactuar con el sistema de archivos *
-/* utilizo el método writeFile de fs para escribir en un archivo */
 
 const url = process.argv.slice(2)[0];
 
 const urlLi = url.split('/');
 const pathUrl = '/' + urlLi[3];
+const hostUrl = urlLi[2];
 
 const https = require('https');
 const options = {
-  hostname: urlLi[2],
-  port: 443,
+  hostname: hostUrl,
   path: pathUrl,
   method: 'GET'
 };
