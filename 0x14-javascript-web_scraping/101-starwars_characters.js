494f53daca65062d3eed9b9cdb370e1da3670e00
Marceloarbiza <3338@holbertonschool.com>
2022-02-02 02:40:16 +0000

    LGF

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -3,26 +3,36 @@
 where the episode number matches a given integer. */
 
 const idFilm = process.argv.slice(2)[0];
-const films = 'https://swapi-api.hbtn.io/api/films/' + idFilm;
+const films = 'https://swapi-api.hbtn.io/api/films/';
+const filmTitle = 'https://swapi-api.hbtn.io/api/films/' + idFilm;
 const request = require('request');
 
 request(films, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
-    let x = 0;
-    const urlJSON = JSON.parse(body);
-    while (urlJSON.characters[x]) {
-      const chara = urlJSON.characters[x];
-      request(chara, function (error, response, body) {
-        if (error) {
-          console.log(error);
-        } else {
-          const charJSON = JSON.parse(body);
-          console.log(charJSON.name);
+    const resultsJSON = JSON.parse(body).results;
+    request(filmTitle, function (error, response, body) {
+      if (error) {
+        console.log(error);
+      } else {
+        const nameJSON = JSON.parse(body);
+        for (const r in resultsJSON) {
+          if (resultsJSON[r].title === nameJSON.title) {
+            const liChar = resultsJSON[r].characters;
+            for (const c in liChar) {
+              request(liChar[c], function (error, response, body) {
+                if (error) {
+                  console.log(error);
+                } else {
+                  const charJSON = JSON.parse(body);
+                  console.log(charJSON.name);
+                }
+              });
+            }
+          }
         }
-      });
-      x++;
-    }
+      }
+    });
   }
 });
