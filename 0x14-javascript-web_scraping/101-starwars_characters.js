e4229150e7a2ebad2b7f40aa225cd0ead26c6f34
Marceloarbiza <3338@holbertonschool.com>
2022-02-02 01:28:00 +0000

    LGF

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+/*  i/ script that prints the title of a Star Wars movie
+where the episode number matches a given integer. */
+
+const idFilm = process.argv.slice(2)[0];
+const films = 'https://swapi-api.hbtn.io/api/films/' + idFilm;
+const request = require('request');
+
+request(films, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    let x = 0;
+    const urlJSON = JSON.parse(body);
+    while (urlJSON.characters[x]) {
+      const chara = urlJSON.characters[x];
+      request(chara, function (error, response, body) {
+        if (error) {
+          console.log(error);
+        } else {
+          const charJSON = JSON.parse(body);
+          console.log(charJSON.name);
+        }
+      });
+      x++;
+    }
+  }
+});
